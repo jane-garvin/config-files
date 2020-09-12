b64b1a46783c5fa7f7e6efd39aef16b6f2866017
John Garvin <john.garvin@gmail.com>
2020-09-12 17:45:38 -0500

    Add whitespace-mode stuff.

--- emacs-common.el
+++ emacs-common.el
@@ -187,6 +187,21 @@ QUOTATION MARK' and `SINGLE COMMA QUOTATION MARK'."
 ;; keep the Mac meaning of the right option key to make symbols like π
 (setq-default mac-right-option-modifier 'none)
 
+;; Make a shortcut for toggling whitespace-mode, and make the indicators
+;; prettier.
+(use-package whitespace
+  :bind ("C-c w" . whitespace-mode)
+  :init
+  (setq whitespace-line-column nil
+        whitespace-display-mappings '((space-mark 32 [183] [46])
+                                      (newline-mark 10 [9166 10])
+                                      (tab-mark 9 [9654 9] [92 9])))
+  :config
+  (set-face-attribute 'whitespace-space       nil :foreground "#cccccc" :background nil)
+  (set-face-attribute 'whitespace-newline     nil :foreground "#cccccc" :background nil)
+  (set-face-attribute 'whitespace-indentation nil :foreground "#cccccc" :background nil)
+  :diminish whitespace-mode)
+
 ;;;; ----- display -----
 
 ;; UTF-8 as default encoding
