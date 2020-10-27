724b37065a14a05fc9607b386d10083138997de7
John Garvin <john.garvin@gmail.com>
2020-10-27 09:12:35 -0500

    For new branch names in git, return to default behavior

--- emacs-common.el
+++ emacs-common.el
@@ -536,8 +536,6 @@ point reaches the beginning or end of the buffer, stop there."
   :commands magit-status magit-blame
   :config
     (setq magit-branch-arguments nil
-          ;; don't put "origin-" in front of new branch names by default
-          magit-default-tracking-name-function 'magit-default-tracking-name-branch-only
           magit-push-always-verify nil
           ;; Get rid of the previous advice to go into fullscreen
           magit-restore-window-configuration t)
