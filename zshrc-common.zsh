459c72222630c512c43cd6613f68cd26d5e75367
John Garvin <john.garvin@gmail.com>
2020-12-31 22:20:36 -0600

    Use XDG Base Directory specification where possible.

--- zshrc-common.zsh
+++ zshrc-common.zsh
@@ -9,3 +9,14 @@ export EDITOR=emacsclient
 PATH+=:/opt/local/bin
 PATH+=:~/Stuff/bin
 PATH+=:/Applications/MacPorts/Emacs.app/Contents/MacOS/bin
+
+# XDG base directory
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+
+export ATOM_HOME="${XDG_DATA_HOME}/atom"
+export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
+export LESSKEY="${XDG_CONFIG_HOME}/less/lesskey"
+export LESSHISTFILE="${XDG_CACHE_HOME}/less/history"
+export XAUTHORITY="${XDG_RUNTIME_DIR}/Xauthority"
